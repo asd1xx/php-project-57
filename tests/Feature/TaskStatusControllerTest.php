4f9cc0031ee9e8b2607b46e5f6b55a919dd2462f
asd2d <872122@mail.ru>
2025-01-29 15:57:56 +0300

    add tests

---
+++ tests/Feature/TaskStatusControllerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\TaskStatus;
+use App\Models\User;
+
+class TaskStatusControllerTest extends TestCase
+{
+    private User $user;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create();
+    }
+
+    public function testIndex(): void
+    {
+        $response = $this->get(route('task_statuses.index'));
+        $response->assertOk();
+    }
+
+    public function testCreate(): void
+    {
+        $response = $this->actingAs($this->user)->get(route('task_statuses.create'));
+        $response->assertOk();
+    }
+
+    public function testEdit(): void
+    {
+        $taskStatus = TaskStatus::factory()->create();
+
+        $response = $this->actingAs($this->user)->get(route('task_statuses.edit', $taskStatus));
+        $response->assertOk();
+    }
+
+    public function testStore(): void
+    {
+        $data = TaskStatus::factory()->make()->only('name');
+        $response = $this->actingAs($this->user)
+            ->post(route('task_statuses.store', $data));
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+        $this->assertDatabaseHas('task_statuses', $data);
+    }
+
+    public function testUpdate(): void
+    {
+        $taskStatus = TaskStatus::factory()->create();
+        $data = TaskStatus::factory()->make()->only('name');
+        $response = $this->actingAs($this->user)->patch(route('task_statuses.update', $taskStatus), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+        $this->assertDatabaseHas('task_statuses', $data);
+    }
+
+    public function testDestroy(): void
+    {
+        $taskStatus = TaskStatus::factory()->create();
+        $response = $this->actingAs($this->user)->delete(route('task_statuses.destroy', $taskStatus));
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+        $this->assertDatabaseMissing('task_statuses', ['id' => $taskStatus->id]);
+    }
+}
