823be38f6b01a52b99c97e391c27d369a55d10ac
asd2d <872122@mail.ru>
2025-02-17 11:12:46 +0300

    create factory

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\TaskStatus;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->sentence(),
+            'description' => fake()->sentence(10),
+            'status_id' => TaskStatus::inRandomOrder()->first(),
+            'created_by_id' => User::inRandomOrder()->first(),
+            'assigned_to_id' => User::inRandomOrder()->first(),
+        ];
+    }
+}
