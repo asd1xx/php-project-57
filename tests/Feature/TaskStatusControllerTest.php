6478108944c97716877f879bfd11349da8efe605
asd2d <872122@mail.ru>
2025-02-17 11:16:26 +0300

    add tests fails

--- tests/Feature/TaskStatusControllerTest.php
+++ tests/Feature/TaskStatusControllerTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\TaskStatus;
 use App\Models\User;
 
 class TaskStatusControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     private User $user;
 
     protected function setUp(): void
@@ -22,28 +25,56 @@ class TaskStatusControllerTest extends TestCase
         $response->assertOk();
     }
 
+    public function testCreateFail(): void
+    {
+        $response = $this->get(route('task_statuses.create'));
+        $response->assertForbidden();
+    }
+
     public function testCreate(): void
     {
         $response = $this->actingAs($this->user)->get(route('task_statuses.create'));
         $response->assertOk();
     }
 
+    public function testEditFail(): void
+    {
+        $taskStatus = TaskStatus::factory()->create();
+        $response = $this->get(route('task_statuses.edit', $taskStatus));
+        $response->assertForbidden();
+    }
+
     public function testEdit(): void
     {
         $taskStatus = TaskStatus::factory()->create();
-
         $response = $this->actingAs($this->user)->get(route('task_statuses.edit', $taskStatus));
         $response->assertOk();
+    }
+
+    public function testStoreFail(): void
+    {
+        $data = TaskStatus::factory()->make()->only('name');
+        $response = $this->post(route('task_statuses.store', $data));
+        $response->assertForbidden();
+        $this->assertDatabaseMissing('task_statuses', $data);
     }
 
     public function testStore(): void
     {
         $data = TaskStatus::factory()->make()->only('name');
-        $response = $this->actingAs($this->user)
-            ->post(route('task_statuses.store', $data));
+        $response = $this->actingAs($this->user)->post(route('task_statuses.store', $data));
         $response->assertSessionHasNoErrors();
         $response->assertRedirect();
         $this->assertDatabaseHas('task_statuses', $data);
+    }
+
+    public function testUpdateFail(): void
+    {
+        $taskStatus = TaskStatus::factory()->create();
+        $data = TaskStatus::factory()->make()->only('name');
+        $response = $this->patch(route('task_statuses.update', $taskStatus), $data);
+        $response->assertForbidden();
+        $this->assertDatabaseMissing('task_statuses', $data);
     }
 
     public function testUpdate(): void
@@ -56,7 +87,23 @@ class TaskStatusControllerTest extends TestCase
         $this->assertDatabaseHas('task_statuses', $data);
     }
 
-    public function testDestroy(): void
+    public function testDeleteFail(): void
+    {
+        $taskStatus = TaskStatus::factory()->create();
+        $response = $this->delete(route('task_statuses.destroy', $taskStatus));
+        $response->assertForbidden();
+        $this->assertDatabaseHas('task_statuses', ['id' => $taskStatus->id]);
+    }
+
+    public function testDeleteFailUsed(): void
+    {
+        $taskStatus = TaskStatus::factory()->hasTasks(1)->create();
+        $response = $this->actingAs($this->user)->delete(route('task_statuses.destroy', $taskStatus));
+        $response->assertRedirect();
+        $this->assertDatabaseHas('task_statuses', ['id' => $taskStatus->id]);
+    }
+
+    public function testDelete(): void
     {
         $taskStatus = TaskStatus::factory()->create();
         $response = $this->actingAs($this->user)->delete(route('task_statuses.destroy', $taskStatus));
